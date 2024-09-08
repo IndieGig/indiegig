2925cd7ace413ad36bed8a29c28b3947d2a69906
vanxh <hello@vanxh.dev>
2024-09-09 01:26:22 +0400

    chore: setup million lint

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
+import MillionLint from "@million/lint";
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {};
-
-export default config;
+export default MillionLint.next({
+	rsc: true,
+})(config);
