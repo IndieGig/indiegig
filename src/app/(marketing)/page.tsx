e6a95c48a0a7e2c171f424dfdd284b472ae03742
vanxh <hello@vanxh.dev>
2024-09-16 02:41:53 +0530

    landing page - features section

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,9 +1,11 @@
+import Features from "./_components/features";
 import Hero from "./_components/hero";
 
 export default async function Page() {
 	return (
-		<main className="container mx-auto space-y-12">
+		<main className="container mx-auto space-y-24">
 			<Hero />
+			<Features />
 		</main>
 	);
 }
