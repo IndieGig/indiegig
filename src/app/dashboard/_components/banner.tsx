29154fa9e1419aa2e394ab743d36f55c63f2758a
vanxh <hello@vanxh.dev>
2024-09-23 00:29:46 +0530

    recommended action card on banner

--- src/app/dashboard/_components/banner.tsx
+++ src/app/dashboard/_components/banner.tsx
@@ -1,13 +1,14 @@
 import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import Image from "next/image";
+import RecommendedActionCard from "./recommended-action-card";
 
 export default async function Banner() {
 	const user = await currentUser();
 
 	return (
-		<div className="text-white p-4 relative h-48">
+		<div className="text-white p-4 relative">
 			<Image
 				src="/demonslayer.webp"
 				alt="Demon Slayer"
@@ -17,17 +18,25 @@ export default async function Banner() {
 			/>
 			<div className="absolute inset-0 bg-gradient-to-b from-black/80 via-black/30 to-black/80 z-[-1]"></div>
 
-			<h3 className="text-2xl font-semibold inline-flex items-center gap-2">
-				Welcome back,
-				<Avatar>
-					<AvatarImage src={user?.imageUrl} />
-					<AvatarFallback>
-						{user?.firstName?.charAt(0)}
-						{user?.lastName?.charAt(0)}
-					</AvatarFallback>
-				</Avatar>
-				{user?.firstName}
-			</h3>
+			<div className="flex flex-col justify-between h-full gap-y-4">
+				<h3 className="text-2xl font-semibold inline-flex items-center gap-2">
+					Welcome back,
+					<Avatar>
+						<AvatarImage src={user?.imageUrl} />
+						<AvatarFallback>
+							{user?.firstName?.charAt(0)}
+							{user?.lastName?.charAt(0)}
+						</AvatarFallback>
+					</Avatar>
+					{user?.firstName}
+				</h3>
+
+				<div className="h-10 lg:h-full"></div>
+
+				<div className="flex items-center gap-4 flex-col lg:flex-row">
+					<RecommendedActionCard />
+				</div>
+			</div>
 		</div>
 	);
 }
---
+++ src/app/dashboard/_components/recommended-action-card.tsx
@@ -0,0 +1,28 @@
+import { WandSparkles } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+export default function RecommendedActionCard() {
+	return (
+		<div className="bg-card max-w-full rounded-lg p-4 flex flex-col gap-y-2 text-card-foreground">
+			<h4 className="text-sm font-semibold uppercase tracking-wide">
+				Recommended for you
+			</h4>
+
+			<div className="flex items-center gap-x-2">
+				<div className="bg-primary text-primary-foreground rounded-full p-3">
+					<WandSparkles className="size-5" />
+				</div>
+
+				<div className="flex flex-col">
+					<h5 className="text-lg font-semibold">Create a new gig</h5>
+					<p className="text-sm text-muted-foreground">
+						Create a new gig to start receiving applications.
+					</p>
+				</div>
+
+				<Button className="ml-6">Create a Gig</Button>
+			</div>
+		</div>
+	);
+}
