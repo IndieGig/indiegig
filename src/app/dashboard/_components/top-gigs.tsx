fb3e372da1e989a5118963ea4a90eb1b64f48a0a
vanxh <hello@vanxh.dev>
2024-09-22 17:19:28 +0530

    carousel component

---
+++ src/app/dashboard/_components/top-gigs.tsx
@@ -0,0 +1,7 @@
+export default function TopGigs() {
+	return (
+		<div className="flex flex-col gap-4">
+			<h2 className="text-2xl font-semibold">Most popular Gigs</h2>
+		</div>
+	);
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,13 @@
 import Banner from "./_components/banner";
+import TopGigs from "./_components/top-gigs";
 
 export default function Page() {
 	return (
-		<main>
+		<main className="flex flex-col gap-4">
 			<Banner />
+			<div className="container mx-auto flex flex-col gap-4">
+				<TopGigs />
+			</div>
 		</main>
 	);
 }
