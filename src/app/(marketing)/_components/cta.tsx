249ea7663a298353082d271a1acb5527a4580772
vanxh <hello@vanxh.dev>
2024-09-22 16:45:27 +0530

    fix

--- src/app/(marketing)/_components/cta.tsx
+++ src/app/(marketing)/_components/cta.tsx
@@ -1,7 +1,3 @@
-import { SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
-import Link from "next/link";
-
-import PulsatingButton from "@/components/magicui/pulsating-button";
 import { Button } from "@/components/ui/button";
 
 export default function CTA() {
@@ -15,20 +11,10 @@ export default function CTA() {
 				IndieGig is your one-stop platform for freelance success.
 			</p>
 
-			<SignedIn>
-				<Link href="/dashboard" passHref className="mt-6">
-					<PulsatingButton>
-						<span className="text-white">View Dashboard</span>
-					</PulsatingButton>
-				</Link>
-			</SignedIn>
-
-			<SignedOut>
-				<div className="flex gap-6 mt-6">
-					<Button>Hire Freelancers</Button>
-					<Button variant="outline">Start Freelancing</Button>
-				</div>
-			</SignedOut>
+			<div className="flex gap-6 mt-6">
+				<Button>Hire Freelancers</Button>
+				<Button variant="outline">Start Freelancing</Button>
+			</div>
 		</div>
 	);
 }
